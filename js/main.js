a6daae853e8c291e8a04210ad0ec343d9064c8db
Gurpreet Singh Bhachu <35268356+gurpreetbhachu@users.noreply.github.com>
2019-02-25 14:36:34 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('.portfolio-box').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		}
+	});
+});
